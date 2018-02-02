15c5dbe54928046248d1527bbae3980678bdebdd
DimiG <dim.georgiev1976@yandex.com>
2018-02-02 12:11:21 -0800

    fuxed styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
+var less = require("gulp-less");
+   
 
-gulp.task('server', ['styles'], function() {
+gulp.task('server', ['less'], function(){
     browserSync.init({
-    	server: { baseDir: './app/'}
+        server: { baseDir: './app/' }
+
     });
-    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('./app/less/**/*.less', ['less']);
-    gulp.watch('./app/sass/**/*.scss', ['sass']);
+
+      gulp.watch('app/**/*.html').on('change', browserSync.reload);
+      gulp.watch('app/less/**/*.less', ['less'] );
+     
 });
 
-gulp.task('styles', function() {
+gulp.task('less', function() {
     return gulp.src('./app/less/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./app/css'))
-    .pipe(browserSync.stream());
+         .pipe(less())
+         .pipe(gulp.dest('./app/css'))
+         .pipe(browserSync.stream());
 });
-
-
 
 gulp.task('default', ['server']);
