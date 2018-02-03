0b17e22f825bd5e29443ca754fd43f1edede4f4e
DimiG <dim.georgiev1976@yandex.com>
2018-02-02 22:18:33 -0800

    made changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ var browserSync = require('browser-sync').create();
 var less = require("gulp-less");
    
 
-gulp.task('server', ['less'], function(){
+gulp.task('server', ['styles'], function(){
     browserSync.init({
         server: { baseDir: './app/' }
 
     });
 
       gulp.watch('app/**/*.html').on('change', browserSync.reload);
-      gulp.watch('app/less/**/*.less', ['less'] );
+      gulp.watch('app/less/**/*.less', ['styles'] );
      
 });
 
-gulp.task('less', function() {
+gulp.task('styles', function() {
     return gulp.src('./app/less/**/*.less')
          .pipe(less())
          .pipe(gulp.dest('./app/css'))
